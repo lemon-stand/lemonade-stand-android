98a5755527e6c94eaac61b11b542242a2249eca4
come219 <55073254+come219@users.noreply.github.com>
2021-09-01 21:12:47 +0100

    Fourth commit to project, new documentation and basic front-end implementation. version 1.01

--- app/src/main/java/com/example/a219_lemonade_stand/MainActivity.java
+++ app/src/main/java/com/example/a219_lemonade_stand/MainActivity.java
@@ -2,12 +2,14 @@ package com.example.a219_lemonade_stand;
 
 /**     Import Libraries   **/
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.BufferedReader;
@@ -15,56 +17,81 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
 
 
 /**
- *  Main Activity Class
+ *  Main Activity Class that allows the user to login and read playerdata
  */
 public class MainActivity extends AppCompatActivity {
 
+
+    //Declaring Views & Variables:
+
     //Images
     private ImageView logo;
+    private ImageView loginscreenimage;
 
     //Define variables which will be used to login.
     private EditText eUsername;
     private EditText ePassword;
     private Button eLogin;
+    private TextView reveal;    // used to help display playerdata.txt
+    boolean isValid = false;    //boolean variable for checking login
 
-    boolean isValid = false;
-
+    //admin user. and pass.
     String adminUsername = "admin";
     String adminPassword = "12345";
 
+    //Current temp variables for functionality
+    String stringsplittemp = "";
+    String usertemp = "";
+    String passtemp = "";
 
+
+    /**
+     *  Main Activity onCreate Function
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
 
-        //Initialising image
+        //Referencing xml elements
+
+        //login screen background image
+        loginscreenimage = (ImageView) findViewById(R.id.loginscreenimageview);
+        loginscreenimage.setImageResource(R.drawable.lemonadestandloginscreen);
+        loginscreenimage.setImageAlpha(30);
+
+        //logo image
         logo = (ImageView) findViewById(R.id.LogoView);
         logo.setImageResource(R.drawable.lemonlogo);
 
-
-        //Referencing xml elements
+        //Username and password elements
         eUsername = findViewById(R.id.etUsername);
         ePassword = findViewById(R.id.etPassword);
         eLogin =  findViewById(R.id.bLogin);
+        reveal = (TextView) findViewById(R.id.revealtext);
 
 
+        /**
+         * Function to read player data file text and allocate the data into the appropriate strings
+         */
         try {
-            InputStream instream = openFileInput("Playerdata.txt");
+            InputStream instream = openFileInput("app/src/main/java/com/example/a219_lemonade_stand/playerdata.txt");
             if (instream != null)
             {
                 InputStreamReader inputreader = new InputStreamReader(instream);
                 BufferedReader buffreader = new BufferedReader(inputreader);
-                String line,line1 = "";
+                String line = "";
                 try
                 {
-                    while ((line = buffreader.readLine()) != ",")
-                        line1+=line;
-                    //usertemp = line;
+                    while ((line = buffreader.readLine()) != ";")
+                        stringsplittemp += line;
+
                 }catch (Exception e)
                 {
                     e.printStackTrace();
@@ -73,10 +100,22 @@ public class MainActivity extends AppCompatActivity {
         }
         catch (Exception e)
         {
+            System.out.println("doesn't read");
             String error="";
             error=e.getMessage();
         }
 
+
+        //allocation of the data into the strings
+        usertemp.equals(stringsplittemp.split(",", 0));
+        System.out.println(usertemp);
+        passtemp.equals(stringsplittemp.split(",", 1));
+        stringsplittemp.substring(usertemp.length(), passtemp.length() );
+        System.out.println(stringsplittemp);
+        passtemp.equals(stringsplittemp);
+
+        //reveal text set
+        reveal.setText(stringsplittemp + "trying to print username and password");
 
         /**
          *Function to compare user input with login data, and to prove authenticity.
@@ -133,9 +172,6 @@ public class MainActivity extends AppCompatActivity {
      * @return          Returns boolean value
      */
     private boolean validate(String name, String password){
-
-        String usertemp = "aa";
-        String passtemp = "bb";
 
         if
         ((name.equals(adminUsername) && password.equals(adminPassword))
